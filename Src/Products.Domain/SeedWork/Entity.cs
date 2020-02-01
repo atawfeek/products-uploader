a51364f07abdca758698211bb0db7cc6b1f37908
Abdelrahman Tawfeek <Abdelrahman.Tawfeek@oireachtas.ie>
2020-02-02 00:49:02 +0200

    create domain project to validate input file before process it in service layer.

---
+++ Src/Products.Domain/SeedWork/Entity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Domain.SeedWork
+{
+    public abstract class Entity
+    {
+        private List<IDomainEvent> _domainEvents;
+
+        /// <summary>
+        /// Domain events occurred.
+        /// </summary>
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents?.AsReadOnly();
+
+        /// <summary>
+        /// Add domain event.
+        /// </summary>
+        /// <param name="domainEvent"></param>
+        protected void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents = _domainEvents ?? new List<IDomainEvent>();
+            this._domainEvents.Add(domainEvent);
+        }
+
+        /// <summary>
+        /// Clear domain events.
+        /// </summary>
+        public void ClearDomainEvents()
+        {
+            _domainEvents?.Clear();
+        }
+
+        /// <summary>
+        /// Get domain events
+        /// </summary>
+        public List<IDomainEvent> GetDomainEvents()
+        {
+            return _domainEvents;
+        }
+    }
+}
---
+++ Src/Products.Domain/SeedWork/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Domain.SeedWork
+{
+    //Marker Interface
+    public interface IAggregateRoot
+    {
+
+    }
+}
