6cef68d729a945c63b214a925275244afc49c630
Abdelrahman Tawfeek <Abdelrahman.Tawfeek@oireachtas.ie>
2020-02-02 01:27:50 +0200

    create base abstraction for domain.

---
+++ Src/Products.Domain/Base/DomainBase.cs
@@ -0,0 +1,21 @@
+﻿using Products.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Products.Domain.Base
+{
+    public abstract class DomainBase : Entity
+    {
+        public DomainBase(string name, int? id = null)
+        {
+            if (id.HasValue)
+                Id = (int)id;
+            Name = name;
+        }
+
+        //Props
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
