e160228b93862bbfa431ae3a9163e25a4426bdbf
Abdelrahman Tawfeek <Abdelrahman.Tawfeek@oireachtas.ie>
2020-02-02 17:17:47 +0200

    add db context, register dbcontext in DI, and configure file storage for next usage of file persistance.

--- Src/Products.Api/Middleware/ServicesMiddleware.cs
+++ Src/Products.Api/Middleware/ServicesMiddleware.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Products.Api.AutoMapper;
 using Products.Dto.Options;
+using Products.Service.Data;
 using Products.Service.Interfaces;
 using Products.Service.Services;
 using System;
@@ -25,19 +28,31 @@ namespace Products.Api.Middleware
         /// <param name="azureStorageOptions"></param>
         /// <param name="azureServiceBusOptions"></param>
         /// <param name="appOptions"></param>
-        public static void ConfigureServices(this IServiceCollection services, 
-                                                IConfiguration configuration, 
+        public static void ConfigureServices(this IServiceCollection services,
+                                                IConfiguration configuration,
                                                 ApplicationSettingsOptions appOptions)
         {
+            // To list physical files from a path provided by configuration:
+            var physicalProvider = new PhysicalFileProvider(configuration.GetValue<string>("ApplicationSettings:StoredFilesPath"));
+            services.AddSingleton<IFileProvider>(physicalProvider);
+
+            var sqlDefaultConnection = string.Empty;
+
+            // Uses Sql Authentication - ** Production usage for  real db instead of InMemory.
+            sqlDefaultConnection = string.IsNullOrEmpty(appOptions.SqlConnectionString) ?
+                                        "connection string is missing." : appOptions.SqlConnectionString;
+
+            //register db context in memory
+            services.AddDbContext<ProductsDbContext>(options => options.UseInMemoryDatabase("InMemoryProductsDb"));
+
             //register services
             services.AddScoped<IProductService, ProductService>();
-            
+
             //register automapper
             services.AddSingleton(provider => new MapperConfiguration(cfg =>
             {
                 cfg.AddProfile(new AutoMapperProfileConfiguration());
             }).CreateMapper());
-
         }
     }
 }
